923d47881092231192da298c404b7135289f3516
ourangelofsorrows <40043442+ourangelofsorrows@users.noreply.github.com>
2018-06-08 13:41:57 +0200

    Update mikabot.py

--- mikabot.py
+++ mikabot.py
@@ -404,29 +404,4 @@ async def on_message(message):
     
 
      
-while True:
-        if heroku:
-            token = os.environ['TOKEN']
-        else:
-            token = get_config_value('config', 'token')
-        try:
-            client.run(token, bot=False)
-        except discord.errors.LoginFailure:
-            if not heroku:
-                if _silent:
-                    print('Cannot use setup Wizard becaue of silent mode')
-                    exit(0)
-                print("It seems the token you entered is incorrect or has changed. If you changed your password or enabled/disabled 2fa, your token will change. Grab your new token. Here's how you do it:\n")
-                print("Go into your Discord window and press Ctrl+Shift+I (Ctrl+Opt+I can also work on macOS)")
-                print("Then, go into the Applications tab (you may have to click the arrow at the top right to get there), expand the 'Local Storage' dropdown, select discordapp, and then grab the token value at the bottom. Here's how it looks: https://imgur.com/h3g9uf6")
-                print("Paste the contents of that entry below.")
-                print("-------------------------------------------------------------")
-                token = input("| ").strip('"')
-                with open("settings/config.json", "r+", encoding="utf8") as fp:
-                    config = json.load(fp)
-                    config["token"] = token
-                    fp.seek(0)
-                    fp.truncate()
-                    json.dump(config, fp, indent=4)
-                continue
-        break
+client.run(os.environ['TOKEN'])
