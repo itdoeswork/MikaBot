9e9360062139293ef2f765cb500c62347aac61bc
ourangelofsorrows <40043442+ourangelofsorrows@users.noreply.github.com>
2018-08-16 19:00:43 +0200

    Update mikabot.py

--- mikabot.py
+++ mikabot.py
@@ -156,8 +156,8 @@ async def resolve_mentions_to_friendly_names(client, server_id, message, member_
 async def submit_quote(client, channel_id, message, message_embeds, message_attachments, author):
     channel = client.get_channel(channel_id)
     #"<:mika:475061308308586527>"
-    embed = discord.Embed(title= "{dragon_head}  **Quote Submitted By:** *{author}*  {dragon_head}".format(author=author, dragon_head="\U0001F432"), 
-            description= ("{dragon} {dragon} {dragon} {dragon} {dragon}\n\n{message}\n\n{dragon} {dragon} {dragon} {dragon} {dragon}" if len(message) > 81 else ("{dragon} {message} {dragon}"  if len(message) > 0 else "")).format(
+    embed = discord.Embed(title= "{dragon_head}  Quote Submitted By: {author}  {dragon_head}".format(author=author, dragon_head="\U0001F432"), 
+            description= ("{message}\n\n{dragon} {dragon} {dragon} {dragon} {dragon}" if len(message) > 81 else ("{dragon} {message} {dragon}"  if len(message) > 0 else "")).format(
                 dragon="\U0001F409", message=message), 
             color=0x7f1ae5)
     result_embed = await merge_embeds(embed, message_embeds, message_attachments)
